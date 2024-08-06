f8a17d426d97e561b05041b68d8e74362dc68216
Josh Elias <josh@daemontech.tools>
2024-08-06 10:53:52 -0400

    feat: optimize recursive sum

--- recursive_sum.go
+++ recursive_sum.go
@@ -1,8 +1,13 @@
 package main
 
 func RecursiveSum(arr []int) int {
-	if len(arr) == 0 {
+	idx := len(arr) - 1
+	return RecursiveSumRecursive(arr, idx)
+}
+
+func RecursiveSumRecursive(arr []int, idx int) int {
+	if idx < 0 {
 		return 0
 	}
-	return arr[0] + RecursiveSum(arr[1:])
+	return arr[idx] + RecursiveSumRecursive(arr, idx-1)
 }
